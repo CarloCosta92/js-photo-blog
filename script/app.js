038aba773e06ff7860c5416d5ff5eaaa3be454a1
Carlo Costa <carlo.costa92@gmail.com>
2025-03-14 14:51:24 +0100

    feat: define endpoint

--- script/app.js
+++ script/app.js
@@ -1,1 +1,19 @@
 // console.log("carlo")
+
+// link API
+const endpoint = "https://lanciweb.github.io/demo/api/pictures/";
+
+// variabile per dove mi devo legare
+const cardContainer = document.getElementById('card-container');
+
+// HTML DA CREARE
+/* <div class="col col-4">
+    <div class="card">
+        <img src="" class="card-img-top" alt="">
+            <div class="card-body">
+                <p class="card-text"></p>
+            </div>
+    </div>
+</div> */
+
+//chiamata api
