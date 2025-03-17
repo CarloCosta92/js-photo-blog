f9de5ad4cec2ddb8a8e323cb73fb20e4e50f1968
Carlo Costa <carlo.costa92@gmail.com>
2025-03-17 15:30:17 +0100

    feat: fix some comments

--- script/app.js
+++ script/app.js
@@ -17,35 +17,35 @@ const bottoneChiudi = document.getElementById('bottone');
 //chiamata api
 
 axios.get(endpoint)
-.then((response)=>{
+  .then((response) => {
 
     const data = response.data;
 
-    for (let i = 0; i< 6;i++){
+    for (let i = 0; i < 6; i++) {
 
-        const card = document.createElement('div');
-      card.classList.add('col', 'col-lg-4' ,'col-md-6','col-sm-12' );  
+      const card = document.createElement('div');
+      card.classList.add('col', 'col-lg-4', 'col-md-6', 'col-sm-12');
       cardContainer.appendChild(card);
 
       const cardMain = document.createElement('div');
-      cardMain.classList.add('card');  
-      card.appendChild(cardMain); 
+      cardMain.classList.add('card');
+      card.appendChild(cardMain);
 
       const img = document.createElement('img');
-      img.src = data[i].url;  
-      img.classList.add('card-img-top');  
-      img.alt = 'immagine';  
+      img.src = data[i].url;
+      img.classList.add('card-img-top');
+      img.alt = 'immagine';
       cardMain.appendChild(img);
 
       const imgPuntina = document.createElement('img');
-      imgPuntina.src = "../img/pin.svg";  
-      imgPuntina.classList.add('puntina');  
-      imgPuntina.alt = 'immagine';  
+      imgPuntina.src = "../img/pin.svg";
+      imgPuntina.classList.add('puntina');
+      imgPuntina.alt = 'immagine';
       cardMain.appendChild(imgPuntina);
 
       const cardBody = document.createElement('div');
-      cardBody.classList.add('card-body'); 
-      cardMain.appendChild(cardBody);  
+      cardBody.classList.add('card-body');
+      cardMain.appendChild(cardBody);
 
       const cardTextDate = document.createElement('p');
       cardTextDate.classList.add('card-text');
@@ -55,24 +55,24 @@ axios.get(endpoint)
       const cardText = document.createElement('h3');
       cardText.classList.add('card-text');
       cardText.textContent = data[i].title;
-      cardBody.appendChild(cardText); 
+      cardBody.appendChild(cardText);
 
     }
-      // devo creare una funzione che al click mi mostri display-block alla classe overlay
-      const elementImg = document.querySelectorAll(".card-img-top");
-      elementImg.forEach((img) => {
-        img.addEventListener("click", function() {
-          overlayDiv.classList.add("overlay");   
-          overlayImage.src = img.src;
-        });
-      });   
+    //funzione che al click mi mostri display-block alla classe overlay
+    const elementImg = document.querySelectorAll(".card-img-top");
+    elementImg.forEach((img) => {
+      img.addEventListener("click", function () {
+        overlayDiv.classList.add("overlay");
+        overlayImage.src = img.src;
+      });
+    });
   });
 
 
 // al click sul bottone tolgo la classe overlay
 
-bottoneChiudi.addEventListener("click", function() {
-overlayDiv.classList.remove("overlay");
+bottoneChiudi.addEventListener("click", function () {
+  overlayDiv.classList.remove("overlay");
 });
 
 
